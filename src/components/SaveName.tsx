c09f449452ad2f090ee82ef2f64dd54be89852e8
mysterion186 <kowsikan@hotmail.fr>
2023-01-13 12:30:08 +0100

    feature : Player name can't be empty now If the player name is empty user can't get to the next page + an error message is rendered under the input

--- src/components/SaveName.tsx
+++ src/components/SaveName.tsx
@@ -1,15 +1,39 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import ParticipationStorage from "../services/ParticipationStorage";
+
 function SaveName() {
-    const location = useLocation();
     const navigate = useNavigate();
+    const [playerName, setPlayerName] = useState("");
+    const [error, setError] = useState(false);
+
+    const handleClick = () => {
+        if (playerName === ""){
+            setError(true);
+        }
+        else {
+            ParticipationStorage.savePlayerName(playerName);
+            navigate("/quiz");
+        }
+    }
+    const handleOnChange = (event:any) => {
+        setError(false);
+        setPlayerName(event.target.value);
+    }
     return (
         <div className="mt-[90%] lg:mt-[20%] flex flex-col justify-center items-centers">
-            <div className="flex items-center border-b border-teal-500 py-2 px-2">
-                <input className="appearance-none bg-transparent border-none w-full text-white-700 text-xl mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" required />           
-                <button onClick={() => {navigate("/quiz")}} className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-4 text-sm text-white py-1 px-2 rounded" type="button">
+            <div className={`flex items-center border-b py-2 px-2 ${error?"border-red-500":"border-teal-500"} `}>
+                <input value={playerName} onChange={handleOnChange} className="appearance-none bg-transparent border-none w-full text-white-700 text-xl mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Jane Doe" aria-label="Full name" required />           
+                <button onClick={handleClick} className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 border-4 text-sm text-white py-1 px-2 rounded" type="button">
                     Commencer
                 </button>      
             </div>
+            {
+                error &&
+                <div className="text-red-500">
+                    Le nom d'utilisateur ne peut être vide 
+                </div>
+            }
         </div>
     )
 }

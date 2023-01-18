9ab4d51f8965b4a121240b8f6864fa03343e6b77
mysterion186 <kowsikan@hotmail.fr>
2023-01-18 09:57:49 +0100

    fix : pseudo-responsiveness -  Add code changement for phone (iphone xr) and large screen only    (nothing inbetween)

--- src/components/QuizDisplay.tsx
+++ src/components/QuizDisplay.tsx
@@ -11,16 +11,17 @@ function QuizDisplay(props : Props) {
     
     return (
         <div className="flex justify-center items-center flex-col">
-            <h1>Quiz display </h1>
-            <img src={props.question.image} alt="Question image" />
+            <img src={props.question.image} alt="Question image" className="mt-[50%] lg:mt-[10%]" />
             <div className="flex justify-center items-center flex-col">
-                <h1>
+                <h1 className="text-xl mt-3 text-center">
                     {props.question.text} - {props.question.position} / {props.total}
                 </h1>
-                <div className="grid grid-cols-1 grid-flow-row  xl:grid-cols-2">
+                <div className="grid grid-cols-1 grid-flow-row  xl:grid-cols-2 mt-3">
                     {
                         props.question.possibleAnswers.map((answer, index : number) => (
-                            <div key={answer.id} className="hover:bg-green-500 py-2 px-1 cursor-pointer" onClick={() => {props.setParentAnswer([...props.userAnswer, index + 1])}} >
+                            <div key={answer.id} 
+                                className="hover:bg-green-500 py-2 px-1 cursor-pointer text-lg text-center" 
+                                onClick={() => {props.setParentAnswer([...props.userAnswer, index + 1])}} >
                                 <p>{answer.text}</p>
                             </div>
                         ))
